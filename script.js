34a62a5b1349acf7caeb9718f5723bda9a0d4d7f
VivianTang11 <vivian.tang@hotmail.se>
2022-10-16 01:29:55 +0200

    modified

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const apiKey = "2a58ec4bbfa26bfe6e0948b24c90bb76";
 const url = `https://api.openweathermap.org/data/2.5`;
 
-let currentDate = new Date().toLocaleDateString();
-
-
 function search() {
   document.getElementById("search").addEventListener("keyup", (e) => {
     if (e.key == "Enter") {
@@ -21,17 +18,33 @@ async function fetchCurrent(city) {
 }
 
 function renderCurrent(data) {
-  document.getElementById("city").innerHTML = `
+  const mainContainer = document.createElement("div")
+  mainContainer = document.setAttribute('id', 'main-container')
+  // const mainContainer = document.getElementById("main-container");
+  let currentDate = new Date().toLocaleDateString();
+
+  const cityBox = document.createElement("div");
+  cityBox.setAttribute("id", "city");
+  cityBox.innerHTML = `
     <h3>Today</h3>
     <span>${currentDate}</span>
     <h2>${data.name}, ${data.sys.country}</h2>`;
-  document.getElementById("icon").innerHTML = `
+
+  const iconBox = document.createElement("div");
+  iconBox.setAttribute("id", "icon");
+  iconBox.innerHTML = `
     <img src=http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png /></img>
     <p>${data.weather[0].description}</p>`;
-  document.getElementById("details").innerHTML = `
+
+  const detailsBox = document.createElement("div");
+  detailsBox.setAttribute("id", "details");
+  detailsBox.innerHTML = `
     <h2 class="main-temp">${data.main.temp} C</h2>
     <span><i class="fa-solid fa-arrow-up"></i> ${data.main.temp_max} C </span>
     <span><i class="fa-solid fa-arrow-down"></i> ${data.main.temp_min} C</span>`;
+
+    const main =  document.getElementsByTagName(main)
+   main.append(mainContainer, cityBox, iconBox, detailsBox);
 }
 
 window.addEventListener("load", search());
