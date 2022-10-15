4f9757449c54b4775154e3c8ac7fc5e5c822c845
VivianTang11 <vivian.tang@hotmail.se>
2022-10-15 09:01:10 +0200

    added todays date

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const apiKey = "2a58ec4bbfa26bfe6e0948b24c90bb76";
 const url = `https://api.openweathermap.org/data/2.5`;
 
+let currentDate = new Date().toLocaleDateString();
+
+
 function search() {
   document.getElementById("search").addEventListener("keyup", (e) => {
     if (e.key == "Enter") {
@@ -20,6 +23,7 @@ async function fetchCurrent(city) {
 function renderCurrent(data) {
   document.getElementById("city").innerHTML = `
     <h3>Today</h3>
+    <span>${currentDate}</span>
     <h2>${data.name}, ${data.sys.country}</h2>`;
   document.getElementById("icon").innerHTML = `
     <img src=http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png /></img>
